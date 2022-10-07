9a1ee18d0585dd273ec6b9b4a2e9ab833b3c42cc
AliAltiyev <alidroid696@gmail.com>
2022-10-07 14:15:24 +0300

    Added details Activity changed styles Added onClick for each item in recycler view

---
+++ app/src/main/java/com/app/lezzet/ui/activity/DetailsActivity.kt
@@ -0,0 +1,28 @@
+package com.app.lezzet.ui.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.app.lezzet.R
+import kotlinx.android.synthetic.main.activity_details.*
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
--- app/src/main/java/com/app/lezzet/ui/adapter/RecipesAdapter.kt
+++ app/src/main/java/com/app/lezzet/ui/adapter/RecipesAdapter.kt
@@ -1,10 +1,13 @@
 package com.app.lezzet.ui.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.ListAdapter
 import com.app.lezzet.databinding.RecyclerViewItemLayoutBinding
 import com.app.lezzet.domain.model.Result
+import com.app.lezzet.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesAdapter : ListAdapter<Result, RecipesViewHolder>(RecipeDiffUtil()) {
 
@@ -18,10 +21,12 @@ class RecipesAdapter : ListAdapter<Result, RecipesViewHolder>(RecipeDiffUtil()) 
     }
 
     override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
-
         val item = getItem(position)
-
+        holder.itemView.setOnClickListener {
+            Log.d("onBindViewHolder", "Data is successfully sent")
+            val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(item)
+            it.findNavController().navigate(action)
+        }
         holder.bind(item)
-
     }
 }
